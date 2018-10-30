b522bcc3a92022632faaa892eb957708a7a029e5
conthoi11796 <38045429+conthoi11796@users.noreply.github.com>
2018-10-30 21:43:33 +0700

    add-product
    - test controller -> them mutil images

--- src/main/java/com/thecoffeshop/Application.java
+++ src/main/java/com/thecoffeshop/Application.java
@@ -6,12 +6,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
+
+import com.thecoffeshop.Service.FileStorageProperties;
 
 @SpringBootApplication
 @ComponentScan({ "com.thecoffeshop" })
 @EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class,
     DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class })
+@EnableConfigurationProperties({
+	FileStorageProperties.class
+})
 public class Application {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/thecoffeshop/test.java
@@ -0,0 +1,15 @@
+package com.thecoffeshop;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class test {
+	@GetMapping(value = { "/test" })
+	public String index() {
+		return "index";
+	}
+}
